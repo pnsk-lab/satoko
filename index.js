fef220836059050dde41e23291f2f7e124388422
Nishi <nishi@nishi.boats>
2026-08-10 06:02:42 +0900

    update path

--- index.js
+++ index.js
@@ -294,7 +294,7 @@ client.once("clientReady", async()=>{
 								fs.renameSync(`2-${out}`, out);
 							}else{
 								try{
-									await execPromise(`/usr/dectalk2/bin/say ${out} ${inf}`);
+									await execPromise(`/usr/dectalk/bin/say ${out} ${inf}`);
 								}catch{
 									f = true;
 								}
